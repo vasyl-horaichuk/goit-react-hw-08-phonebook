b0a01ca30bd23faa58b03160b39ad7a0d89903d7
vasyl-horaichuk <vg.createam@gmail.com>
2023-03-07 08:59:17 +0000

    Deploying to gh-pages from @ vasyl-horaichuk/goit-react-hw-08-phonebook@e65d42e6281fd314d5a4c36beae2399693c25bdc 🚀

---
+++ static/js/285.4ad7a4c1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[285],{285:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:" PAGE TRGISTER"})}}}]);
+//# sourceMappingURL=285.4ad7a4c1.chunk.js.map
