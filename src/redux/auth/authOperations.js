18faf3d308684012013e84e7c29af76308cd053c
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-07 16:01:17 +0200

    update

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -19,7 +19,6 @@ export const register = createAsyncThunk(
     try {
       const res = await axios.post('/users/signup', credentials);
       setAuthHeader(res.data.token);
-      console.log(res);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { register } from './authOperations';
-
-//  logIn, logOut, refreshUser;
+import { register, logIn, logOut, refreshUser } from './authOperations';
 
 const initialState = {
   user: { name: null, email: null },
@@ -14,8 +12,21 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: builder => {
-    return builder.addCase(register.fulfilled, (state, action) => {
+    builder.addCase(register.fulfilled, (state, action) => {
       state.token = action.payload.token;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(logIn.fulfilled, (state, action) => {
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(logOut.fulfilled, (state, action) => {
+      state.token = '';
+      state.isLoggedIn = false;
+    });
+    builder.addCase(refreshUser.fulfilled, (state, action) => {
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
     });
   },
 });
