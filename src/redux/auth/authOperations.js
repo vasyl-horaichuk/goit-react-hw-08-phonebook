efc46994b228cbd357b65d1db0fe88d875088e97
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-07 16:31:47 +0200

    update

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 // При реєстрації або при логіні отримуємо токен
 const setAuthHeader = token => {
