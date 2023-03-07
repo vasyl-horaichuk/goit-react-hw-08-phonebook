f73a1fa9195710c530578f0010311edd15856a31
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-07 10:28:31 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './cotactForm/contactsSlice';
 import { filterReducer } from './cotactForm/filterSlice';
 import { authReducer } from './auth/authSlice';
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    auth: authReducer,
-    filter: filterReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+};
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  auth: authReducer,
+  filter: filterReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
