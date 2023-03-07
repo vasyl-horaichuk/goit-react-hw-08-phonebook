dcbcb5be39750518cafd6758d0d06af4917573d9
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-07 23:13:48 +0200

    update

--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -1,15 +1,29 @@
 import { ContactItemButton } from './ContactItem.styled';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const ContactItem = ({ contact: { id, name, number }, onDelete }) => {
+  const [isEdid, setIsEdit] = useState(false);
+
+  const handleChangeMode = () => {
+    if (isEdid) {
+      setIsEdit(prev => !prev);
+      return;
+    }
+  };
   return (
     <>
-      <p>
+      {isEdid ? <input type="text" value={name} /> : <p>{name}: </p>}
+      {isEdid ? <input type="tel" value={number} /> : <p>{number}</p>}
+      {/* <p>
         {name} : {number}
-      </p>
+      </p> */}
       <ContactItemButton type="button" onClick={() => onDelete(id)}>
         Delete
       </ContactItemButton>
+      <button type="button" onClick={() => {}}>
+        {isEdid ? 'Save' : 'Edid'}
+      </button>
     </>
   );
 };
