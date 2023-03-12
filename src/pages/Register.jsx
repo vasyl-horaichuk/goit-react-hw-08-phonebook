53cffef6f71501110aadc5844b8e39d3b8537a54
Vasy Horaichuk <vg.createam@gmail.com>
2023-03-12 14:34:00 +0200

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,12 @@
 import { RegisterForm } from 'components/RegisterFrom/RegisterForm';
+import {Helmet} from 'react-helmet';
 
 const RegisterPage = () => {
   return (
     <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
       <h1>PAGE REGISTER</h1>
       <RegisterForm />
     </div>
