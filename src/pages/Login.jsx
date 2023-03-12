57dee75454962c95079eb48155f8577ad696250b
Vasy Horaichuk <vg.createam@gmail.com>
2023-03-12 14:42:00 +0200

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,12 @@
+import {Helmet} from 'react-helmet';
 import { LoginForm } from 'components/LoginForm/LoginForm';
 
 const LoginPage = () => {
   return (
     <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
       <h1>PAGE LOGIN</h1>
       <LoginForm />
     </div>
